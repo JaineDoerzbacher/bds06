d0fb4d47d14e9947cdf964b2bc47dacce7ad6982
jl-do <doerzbacherjiaine@gmaill.com>
2023-07-12 20:59:20 -0300

    Exercicio sql

---
+++ src/main/java/com/devsuperior/movieflix/controller/ReviewController.java
@@ -0,0 +1,54 @@
+package com.devsuperior.movieflix.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.devsuperior.movieflix.dtos.ReviewDTO;
+import com.devsuperior.movieflix.services.AuthService;
+import com.devsuperior.movieflix.services.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import com.devsuperior.movieflix.entities.User;
+
+
+@RestController
+@RequestMapping(value = "/reviews")
+public class ReviewController {
+
+    @Autowired
+    private ReviewService service;
+
+    @Autowired
+    private AuthService authService;
+
+    @GetMapping
+    public ResponseEntity<List<ReviewDTO>> findAll() {
+
+        User user = authService.authenticated();
+        authService.validateSelfOrMemberAndVisitor(user.getId());
+
+        List<ReviewDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @Transactional(readOnly = true)
+    @PreAuthorize("hasAnyRole('MEMBER')")
+    @PostMapping
+    public ResponseEntity<ReviewDTO> insert(@Valid @RequestBody ReviewDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body((dto));
+    }
+
+}
---
+++ src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -0,0 +1,52 @@
+package com.devsuperior.movieflix.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
+
+import com.devsuperior.movieflix.dtos.ReviewDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.repositories.ReviewRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository repository;
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private AuthService authService;
+
+    @Transactional(readOnly = true)
+    public List<ReviewDTO> findAll() {
+        List<Review> list = repository.findAll();
+        return list.stream().map(x -> new ReviewDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public ReviewDTO insert(ReviewDTO dto) {
+        User user = authService.authenticated();
+
+        try {
+            Review entity = new Review();
+            entity.setMovie(movieRepository.getOne(dto.getMovieId()));
+            entity.setUser(user);
+            entity.setText(dto.getText());
+            repository.save(entity);
+            return new ReviewDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found " + dto.getMovieId());
+        }
+
+    }
+}
